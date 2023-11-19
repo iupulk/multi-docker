c7d2921b32d4f90f200f1bf5316b7406b992ff67
iupulk <iupulk@gmail.com>
2023-11-19 02:13:17 +0000

    changed db authentication

--- server/index.js
+++ server/index.js
@@ -16,13 +16,19 @@ const pgClient = new Pool({
     host: keys.pgHost,
     database: keys.pgDatabase,
     password: keys.pgPassword,
-    port:keys.pgPort
+    port: keys.pgPort,
+    ssl:
+        process.env.NODE_ENV !== 'production'
+            ? false
+            : { rejectUnauthorized: false },
 });
 pgClient.on('error', () => console.log('Lost PG connection'));
 
-pgClient
-.query('CREATE TABLE IF NOT EXISTS values (number INT)')
-.catch(err => console.log(err));
+pgClient.on('connect', (client) => {
+    client
+        .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+        .catch((err) => console.log(err));
+});
 
 //Redis client set up
 const redis = require('redis');
